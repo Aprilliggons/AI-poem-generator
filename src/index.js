79fea4ba7a59ca9ad362d2efc2ce742bf811ce04
Aprilliggons <149065998+Aprilliggons@users.noreply.github.com>
2024-02-18 10:00:49 -0500

    Improved the User experience

--- src/index.js
+++ src/index.js
@@ -18,6 +18,9 @@ function generatePoem(event) {
     "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem and separate each line with <br /> . Make sure to follow the user instructions. Do not include a title. ";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
